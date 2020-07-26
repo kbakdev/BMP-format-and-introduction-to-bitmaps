3cd1b628f1467a216b179e22fd1db8387e29baeb
Sejki <bonczalke@gmail.com>
2020-07-26 21:19:13 +0200

    little fix

--- gradient.py
+++ gradient.py
@@ -21,15 +21,15 @@ for y in range(H):
 # "Manually" copy the lines from the above array to the pygame frame buffer.
 
 pixels = pygame.PixelArray(window)
-center_x = (WINDOW_W - W) / 2
-center_y = (WINDOW_H - H) / 2
+center_x = (WINDOW_W - W) // 2
+center_y = (WINDOW_H - H) // 2
 for y in range(H):
     for x in range(W):
         # PyGame uses BGR format, I used RGB on purpose in the gradient buffer.
         # The following is an RGB to BGR conversion
         # (assuming the 'L' in array.array is Little Endian).
         pixel = gradient[x + y * W]
-        pixel = ((pixel & 0x0000ff << 16) | # R is shifted to the top byte.
+        pixel = (((pixel & 0x0000ff) << 16) | # R is shifted to the top byte.
                  (pixel & 0x00ff00) |        # G remains unchanged.
                  ((pixel & 0xff0000) >> 16)) # B is shifted to the lower byte.
 
